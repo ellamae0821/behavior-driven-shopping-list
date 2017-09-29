fd8b9339956b515eb6138b30727f1de25ecd326a
Ella Mae A <ellamae207@gmail.com>
2017-09-28 16:35:23 -1000

    passes 12 of 13 tests

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -46,16 +46,46 @@ class ShoppingListItem {
     return itemStr;
 
 */
+    var itemStr = `<li class="checkbox" type="checkbox" ${this.is_done ? "checked" : ""}>\n<span>${this.name}</span>\n<span>${this.description}</span></li>`;
+    return itemStr;
+  }
+}
+module.exports=ShoppingListItem;
+
+/*
+AssertionError: expected
+'<li class="checkbox" type="checkbox" >\n<span>shawarma</span>\n<span>yummy</span></li>\n    ' to equal
+'<li class="completed_false"><span>shawarma</span>&nbsp;<span>yummy</span></li>'
+    at Context.<anonymous> (js/shopping_list_test.js:53:30)
+
+
+<li class="completed_false"><span>shawarma</span>&nbsp;<span>yummy</span></li>
+      expect(testItemList.render()).to.equal(`<ul><li class="completed_false"><span>Beer</span>&nbsp;<span>Party</span></li><li class="completed_false"><span>Lays</span><span>Snack</span></li></ul>`);
+
+
+    expect(milk.render()).to.equal(`<li class="completed_${milk.is_done}"><span>${milk.name}</span> <span>${milk.description}</span>`);
+
+
+
+      render() {
     var itemStr = `
     <input class="checkbox" type="checkbox" id="${this.id}" ${this.is_done ? "checked" : ""}>\n
     <span>${this.name}</span>\n
     <span>${this.description}</span>
     `;
     return itemStr;
-  }
-}
-module.exports=ShoppingListItem;
+
+
+    //`<li class="completed_${this.is_done}"><input class="checkbox" type="checkbox" id="${this.id}"><span>${this.name}</span> <span>${this.description}</span>`
 
 
 
+AssertionError:
+expected '\n    <input class="checkbox" type="checkbox" >\n\n    <span>shawarma</span>\n\n    <span>yummy</span>\n    '
+
+
+to equal '<li class="completed_false"><span>shawarma</span> <span>yummy</span>'
+    at Context.<anonymous> (js/shopping_list_test.js:56:30)
+
+*/
 
--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -10,7 +10,7 @@ const expect = chai.expect;
 describe('Shopping List Item', function (){
   let item;
   beforeEach (function () {
-    item = new ShoppingListItem('shawarma', 'yummy',);
+    item = new ShoppingListItem('shawarma', 'yummy');
   });
 //PROPERTY
   it('Expect Shopping List Item should be a Class', function (){
@@ -44,23 +44,15 @@ describe('Shopping List Item', function (){
     item.uncheck();
     expect(item.is_done).to.equal(false);
   });
-/*
-  it('Should have a method named render', function() {
+
+  it('has a method named render', function() {
     expect(item.render).to.be.a('function');
     expect(item.render()).to.be.a('string');
-    expect(item.render()).to.equal(`<li class="completed_${shawarma.is_done}"><span>${shawarma.name}</span> <span>${shawarma.description}</span>`);
+/*    expect(item.render()).to.equal(`<li class="completed_${item.is_done}"><span>${item.name}</span> <span>${item.description}</span>`);
 */
-  it('has a method named render', function() {
-    expect(shawarma.render).to.be.a('function');
-    expect(shawarma.render()).to.be.a('string');
-    expect(shawarma.render()).to.equal(`<li class="completed_${shawarma.is_done}"><span>${shawarma.name}</span> <span>${shawarma.description}</span>`);
-/*  it('has a method named render', function() {
-    expect(milk.render).to.be.a('function');
-    expect(milk.render()).to.be.a('string');
-    expect(milk.render()).to.equal(`<li class="completed_${milk.is_done}"><span>${milk.name}</span> <span>${milk.description}</span>`);*/
+    expect(item.render()).to.equal(`<li class="completed_${item.is_done}"><span>${item.name}</span> <span>${item.description}</span>`);
     });
 
-//  });
 });
 
 
@@ -92,37 +84,26 @@ describe('Shopping List', function (){
     sl.addItem(newSLI);
     expect(sl.items.length).to.equal(1);
     expect(sl.items[0]).to.be.an.instanceof(ShoppingListItem);
-
   });
 
-  describe('"removeItem"', function (){
+//  describe('"removeItem"', function (){
     var juice = new ShoppingListItem("OrangeJuice", "quencher");
     var eggs = new ShoppingListItem("Eggs", "chickenPoop");
+//    var sl = new ShoppingList('bread');
 
     it('Should have a method `removeItem`', function (){
       expect(sl.removeItem).to.be.a('function');
     });
     it('Should remove the object passed in from the `items` array', function (){
-      sl = new ShoppingList('bread');
+
       sl.addItem(juice);
       sl.addItem(eggs);
       sl.removeItem(juice);
       expect(sl).to.not.include(juice);
-      expect(sl.removeItem()).to.equal(items = items.pop());
-//      sl.items.to.not.contain(juice);
+      expect(sl.removeItem()).to.equal(sl.items = sl.items.pop());
     });
-  });
 
-/*
 
-  it('has a method removeItem', function() {
-    var temp = new ShoppingListItem('milk', 'cows');
-    cheese.addItem(temp);
-    cheese.removeItem(temp);
-    expect(cheese).to.not.include(temp);
-    expect(cheese.removeItem()).to.equal(items = items.pop());
-    var temp2;
-    expect(cheese.removeItem(temp2).to.be.an('error'));
-  });*/
+
 
 });
